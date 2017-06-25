032f084c12bca0780d690598f183f9babe56b5e8
Daniele <daniele.stirp@gmail.com>
2017-06-25 12:03:07 +0200

    Bind Fragments to view

--- app/src/main/java/com/example/daniele/testgooglebooks/BookDetailFragment.java
+++ app/src/main/java/com/example/daniele/testgooglebooks/BookDetailFragment.java
@@ -1,6 +1,11 @@
 package com.example.daniele.testgooglebooks;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by daniele on 25/06/17.
@@ -11,5 +16,12 @@ public class BookDetailFragment extends Fragment {
     public static BookDetailFragment newInstance() {
         BookDetailFragment fragment = new BookDetailFragment();
         return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_book_detail, container, false);
+        return rootView;
     }
 }
--- app/src/main/java/com/example/daniele/testgooglebooks/BooksListFragment.java
+++ app/src/main/java/com/example/daniele/testgooglebooks/BooksListFragment.java
@@ -1,6 +1,11 @@
 package com.example.daniele.testgooglebooks;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by daniele on 25/06/17.
@@ -11,5 +16,12 @@ public class BooksListFragment extends Fragment {
     public static BooksListFragment newInstance() {
         BooksListFragment fragment = new BooksListFragment();
         return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_books_list, container, false);
+        return rootView;
     }
 }
